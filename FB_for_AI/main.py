2afa4665ccc5071ad52f0a77a72813da7bc39448
Wanxuan Li <lzlu1018@gmail.com>
2022-09-28 19:15:12 +0800

    AI player can play; still have bugs

--- FB_for_AI/main.py
+++ FB_for_AI/main.py
@@ -1,35 +1,33 @@
 # import modules
-import AI
+import AI as AI
 import FB_for_AI as FB
 import threading
-from time import sleep
 from pynput.keyboard import Controller
+from time import sleep
+
 
 #________________________________________________________________
-#   Thread 1: Create and run the game
+#   Thread 1: Create and run the game 10 times every generation
 #   Thread 2: AI mimic keyboard input
 
-# AI will modify this global variable to decide whether to jump.
-should_jump = True
-
-# create and run a new game, exit when gameover
+# create and run a new game, exit when gameover; For every generation
 def StartGame():
-    game = FB.Game()
-    game.CreateGame()
-    if game.is_game_running == False: return
+    for i in range(10):
+        player = FB.AIPlayer(True)
+        player.CreateGame()
+    return
 
 # when game is not over, mimic keyborad input; exit when gameover
 def StartPlay():
-    global should_jump
     keyboard = Controller()
     key = " "
 
     while True:
-        if gameThread.is_alive() and should_jump == True:
+        if gameThread.is_alive() and FB.should_jump == True:
             keyboard.press(key)
             keyboard.release(key)
             sleep(0.4)
-        elif gameThread.is_alive() and should_jump == False: pass
+        elif gameThread.is_alive() and FB.should_jump == False: pass
         else: return
 
 # Multithread class: call different functions based on thread name
@@ -45,15 +43,12 @@ class AIThread(threading.Thread):
         elif self.name == "AIThread":   StartPlay()
         print("Exiting " + self.name)
 
-
 #_________________________________________________________________
 #   Main Function for testing (Will be commented out later)
-
 
 if __name__ == "__main__":
     gameThread = AIThread(1, "GameThread")
     aiThread = AIThread(2, "AIThread")
-
     gameThread.start()
     aiThread.start()
 
